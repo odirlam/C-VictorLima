c72b85065e282bc1518fe08ba6d9d1964615d85e
odirlam <odirlam@hotmail.com>
2022-05-09 15:02:27 -0300

    Primeiro Commit Victor

---
+++ exerciciosVictorLima/CursoCsharpVictorLima/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CursoCsharpVictorLima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            float velocidade = 248.92f;
+            funcMSG();
+        }
+        static void funcMSG()
+        {
+            Console.WriteLine("Oi eu sou a função");
+        }
+    }
+}
